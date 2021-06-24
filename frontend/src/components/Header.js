3f16de5a1740d534f753eedef9d13c451a5bfae2
jackieung451 <jackieung451@gmail.com>
2021-06-23 19:19:09 -0700

    Need updates on Header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,6 +9,10 @@ const Header = () => {
               <Navbar.Brand href="/">ProShop</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
+
+
+
+              {/* Need to fix the header */}
                 <Nav className="ml-auto">
                   <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
                   <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
